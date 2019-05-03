3f5a4918b805dbefa7eac1b959feade9e2a75d21
TALHA MUSTAFA <talha.mustafa@sleepwellsystem.com>
2019-05-03 07:38:28 +0500

    button pulled up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAmuPxdhCdTfBbrSTHPHtA6Fx4viyOjag8",
+    authDomain: "advanced-attendance-system.firebaseapp.com",
+    databaseURL: "https://advanced-attendance-system.firebaseio.com",
+    projectId: "advanced-attendance-system",
+    storageBucket: "advanced-attendance-system.appspot.com",
+    messagingSenderId: "887905178618"
+  }
 };
